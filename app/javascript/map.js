e872d193bffb1fe7f43a88a8a4609c96ad913a45
Christian St. John <chris@stjohnstudios.com>
2025-07-23 22:17:11 -0500

    Adds house icon as marker

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -27,10 +27,14 @@ async function initMap() {
   const sites = document.querySelectorAll("li.site-list-item");
   sites.forEach(site => {
     // console.log("site is ", site);
-    // Hide the glyph.
-    const pinNoGlyph = new google.maps.marker.PinElement({
-      glyph: "",
-    });
+    // Create custom house marker
+    const markerContent = document.createElement('div');
+    markerContent.className = 'custom-marker';
+    markerContent.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" class="house-icon">
+        <path d="M19.07,4.93C17.22,3 14.66,1.96 12,2C9.34,1.96 6.79,3 4.94,4.93C3,6.78 1.96,9.34 2,12C1.96,14.66 3,17.21 4.93,19.06C6.78,21 9.34,22.04 12,22C14.66,22.04 17.21,21 19.06,19.07C21,17.22 22.04,14.66 22,12C22.04,9.34 21,6.78 19.07,4.93M17,12V18H13.5V13H10.5V18H7V12H5L12,5L19.5,12H17Z" fill="#2563eb"/>
+      </svg>
+    `;
 
     const position = {
       lat: parseFloat(site.getAttribute("data-latitude")),
@@ -46,7 +50,7 @@ async function initMap() {
       map: map,
       gmpClickable: true,
       gmpDraggable: false,
-      content: pinNoGlyph.element
+      content: markerContent
     });
 
     // Add event listeners to the marker's element/content
@@ -57,7 +61,7 @@ async function initMap() {
     marker.content.addEventListener("mouseover", () => {
       const newContent = buildContent(site);
       newContent.addEventListener("mouseout", () => {
-        marker.content = pinNoGlyph.element;
+        marker.content = markerContent;
       });
       marker.content = newContent;
     });
